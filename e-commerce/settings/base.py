f414e82f1d52abc1d333e818d9e5bb7bda96066f
bappi2016 <bappi.ajmalaamir@gmail.com>
2019-09-09 23:09:09 +0600

    add secret key

--- e-commerce/settings/base.py
+++ e-commerce/settings/base.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = jx@fm!+yqselbaqp^z&hnckmj$d*!lxmtl-rlsezy-(-bct=k%
 
 
 
