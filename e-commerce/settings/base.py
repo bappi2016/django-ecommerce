8301681ef3e4755364df3204be6a052a297a72ef
bappi2016 <bappi.ajmalaamir@gmail.com>
2019-09-09 22:53:57 +0600

    First Commit

--- e-commerce/settings/base.py
+++ e-commerce/settings/base.py
@@ -84,6 +84,14 @@ TEMPLATES = [
     },
 ]
 
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
 WSGI_APPLICATION = 'e-commerce.wsgi.application'
 
 
@@ -148,10 +156,11 @@ LOGIN_REDIRECT_URL = '/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 
+# heroku setup
 #  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-
+# heroku setup
 import dj_database_url
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
