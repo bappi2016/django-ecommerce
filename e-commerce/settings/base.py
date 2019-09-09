1355f6d702b7ebfc672d84353c88e2fb93d5f55c
bappi2016 <bappi.ajmalaamir@gmail.com>
2019-09-09 23:45:09 +0600

    A sc

--- e-commerce/settings/base.py
+++ e-commerce/settings/base.py
@@ -16,12 +16,14 @@ from decouple import config
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+SECRET_KEY = 'jx@fm!+yqselbaqp^z&hnckmj$d*!lxmtl-rlsezy-(-bct=k%'
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = jx@fm!+yqselbaqp^z&hnckmj$d*!lxmtl-rlsezy-(-bct=k%
+
 
 
 
